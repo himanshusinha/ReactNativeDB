2f089c5432bedd6c2741ca754f0cebfd00babd30
hsinha1110 <himanshusinha1110@gmail.com>
2026-02-08 13:44:40 +0530

    mergeItem

--- App.tsx
+++ App.tsx
@@ -11,45 +11,72 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const App = () => {
   const [input, setInput] = useState<string>('');
   const [savedData, setSavedData] = useState<string>('');
+  const USER_1 = {
+    name: 'Tom',
+    age: 20,
+    traits: {
+      hair: 'black',
+      eyes: 'blue',
+    },
+  };
+
+  const USER_2 = {
+    name: 'Sarah',
+    age: 21,
+    hobby: 'cars',
+    traits: {
+      eyes: 'green',
+    },
+  };
 
   const saveData = async () => {
     try {
-      const data = await AsyncStorage.setItem('data', input);
-      setSavedData(JSON.stringify(data));
+      await AsyncStorage.setItem('data', JSON.stringify(USER_1));
+      await AsyncStorage.mergeItem('data', JSON.stringify(USER_2));
+      const currentUser = await AsyncStorage.getItem('data');
+
+      setSavedData(input);
+      if (currentUser !== null) {
+        const parseValue = JSON.parse(currentUser); 
+        console.log('Merged Data:', parseValue);
+      }
     } catch (e) {
       console.log('Error saving data', e);
     }
   };
 
-  const getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem('data');
-      if (value !== null) {
-        setSavedData(value);
-      }
-    } catch (e) {
-      console.log('Error getting data', e);
-    }
-  };
+  // const getData = async () => {
+  //   try {
+  //     const value = await AsyncStorage.getItem('data');
+  //     if (value !== null) {
+  //       const parsedValue: string = JSON.parse(value);
+  //       setGetSavedData(parsedValue);
+  //     }
+  //   } catch (e) {
+  //     console.log('Error getting data', e);
+  //   }
+  // };
 
   return (
     <View style={styles.container}>
-      <TextInput
+      {/* <TextInput
         value={input}
         onChangeText={setInput}
         style={styles.input}
         placeholder="Enter value"
-      />
+      /> */}
 
       <TouchableOpacity style={styles.button} onPress={saveData}>
         <Text style={styles.title}>Save Data</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} onPress={getData}>
+      {/* <TouchableOpacity style={styles.button} onPress={getData}>
         <Text style={styles.title}>Get Data</Text>
       </TouchableOpacity>
 
-      <Text style={styles.titleInfo}>Stored Data : {savedData}</Text>
+      <Text style={styles.titleInfo}>Save Data : {savedData}</Text>
+
+      <Text style={styles.titleInfo}>Get Data : {getSavedData}</Text> */}
     </View>
   );
 };
